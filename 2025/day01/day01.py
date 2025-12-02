f06fe2ec80c76c089c1b982ee15c2122f0710cb2
Johni12M <Johni12M@users.noreply.github.com>
2025-12-02 11:36:19 +0100

    day01

--- 2025/day01/day01.py
+++ 2025/day01/day01.py
@@ -1,6 +1,26 @@
-with open("input.txt", "r", encoding="utf-8") as file:
-    input_data = file.readlines()
+class Solution:
+    def part1(self):
+        with open("input.txt", "r", encoding="utf-8") as f:
+            lines = f.read().splitlines()
+        ans = 0
+        dial = 50
+        for line in lines:
+            direction = line[0]
+            count = int(line[1:])
+            if direction == "L":
+                dial -= count
+            else:
+                dial += count
+            dial %= 100  # wrap around 0-99
 
-processed_lines = [line.split("\n") for line in input_data]
+            if dial == 0:
+                ans += 1
+        return ans
 
-print(processed_lines)
+    def part2(self):
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.part1())
